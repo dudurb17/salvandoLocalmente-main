246dcd596ed294dfc7fcf0062b5852885050d5f0
dudurb17 <eduardo.robettibedin@gmail.com>
2024-03-27 08:40:13 -0300

    criando id dinamico, mas não re comendado

--- src/componentes/NotaEditor.js
+++ src/componentes/NotaEditor.js
@@ -15,17 +15,23 @@ export default function NotaEditor() {
   const [modalVisivel, setModalVisivel] = useState(false);
 
   async function salvaNota() {
+    const novoId = await geraId();
     const umaNota = {
-      id: "1",
+      id: novoId.toString(),
       texto: texto,
     };
+    console.log(umaNota);
     await AsyncStorage.setItem(umaNota.id, umaNota.texto);
     mostraNota();
   }
-
-  async function mostraNota() {
-    console.log(await AsyncStorage.getItem("1"));
+  async function geraId() {
+    const todasChaves = await AsyncStorage.getAllKeys();
+    if (todasChaves <= 0) {
+      return 1;
+    } else return todasChaves.length + 1;
   }
+
+  async function mostraNota() {}
 
   return (
     <>
